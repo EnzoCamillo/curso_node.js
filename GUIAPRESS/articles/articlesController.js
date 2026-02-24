dfa8f9b9ca4b1ed7365aa5156bc3defad51c2ce9
Enzo Gabriel Camillo <enzocamillo456@gmail.com>
2026-02-23 22:25:42 -0300

    Finalizando projeto do curso.

--- GUIAPRESS/articles/articlesController.js
+++ GUIAPRESS/articles/articlesController.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const Category = require('../categories/Category');
 const Article = require("./Article");
 const slugify = require("slugify");
+const adminAuth = require("../middlewares/adminAuth");
 
 
-router.get("/admin/articles", (req, res) => {
+router.get("/admin/articles", adminAuth , (req, res) => {
     Article.findAll({
         include: [{model: Category}]
     }).then(articles => {
@@ -13,7 +14,7 @@ router.get("/admin/articles", (req, res) => {
     });
 });
 
-router.get("/admin/articles/new", (req, res) => {
+router.get("/admin/articles/new", adminAuth ,(req, res) => {
     Category.findAll().then(categories => {
         res.render("admin/articles/new",{categories: categories});
     });
@@ -89,5 +90,40 @@ router.post("/articles/update", (req, res) => {
         res.redirect("/");
     })
 });
+
+router.get("/articles/page/:num", (req, res) => {
+    var page = req.params.num;
+
+    if(isNaN(page) || page == 1){
+        var offset = 0;
+        console.log("roma");
+    }else{
+        var offset = (parseInt(page) - 1) * 4;
+    }
+
+    Article.findAndCountAll({
+        limit: 4,
+        offset: offset,
+                order: [
+            ['id', 'DESC']
+        ],
+    }).then(articles => {
+        var next;
+        if (offset + 4 >= articles.count){
+            next = false;
+        }else{
+            next = true;
+        }
+        var result = {
+            page: parseInt(page),
+            next: next,
+            articles: articles,
+        };
+
+        Category.findAll().then(categories => {
+            res.render("admin/articles/page", {result: result, categories: categories});
+        });
+    })
+})
 
 module.exports = router;
--- GUIAPRESS/index.js
+++ GUIAPRESS/index.js
@@ -1,17 +1,25 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const session = require("express-session");
 const connection = require('./database/database');
 
-const categoriesController = require('./categories/categoriesController');
-const articlesController = require('./articles/articlesController');
+const categoriesController = require('./categories/CategoriesController');
+const articlesController = require('./articles/ArticlesController');
+const usersController = require('./users/UsersController');
 
 const Article = require("./articles/Article");
 const Category = require("./categories/Category");
+const User = require("./users/User")
 const { where } = require("sequelize");
 
 // View Engine
 app.set('view engine','ejs');
+
+// Sessions
+app.use(session({
+    secret: "qualquercoisa", cookie: {maxAge: 30000000}
+}));
 
 // static
 app.use(express.static('public'));
@@ -30,15 +38,17 @@ connection
     });
 
 
-    app.use("/", categoriesController);
-    app.use("/", articlesController);
+app.use("/", categoriesController);
+app.use("/", articlesController);
+app.use("/", usersController);
 
 app.get("/", (req, res) => {
 
     Article.findAll({
         order: [
             ['id', 'DESC']
-        ]
+        ],
+        limit: 4
     }).then(articles =>{
 
         Category.findAll().then(categories => {
